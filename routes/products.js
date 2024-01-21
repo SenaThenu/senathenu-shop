518a41ec96a96f24e67517077e78f5b53ea36ed1
Senash Thenuja <senathenu@gmail.com>
2024-01-22 00:10:20 +1300

    set up the server to read all the product images

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const fsPromise = require("fs").promises;
+const path = require("path");
+
+async function readProductImages(imagesPath) {
+    try {
+        const images = await fsPromise.readdir(imagesPath);
+        return images;
+    } catch (err) {
+        console.error("Error reading folder:", err);
+    }
+}
 
 router.get("/:product", (req, res) => {
     let product = req.params.product;
     product = product.toLowerCase().replaceAll("-", "_");
 
     if (product in global.productsData) {
-        res.render("product", {
-            title: `${global.productsData[product]["display_name"]} - SenaVerse Shop`,
-            ...global.productsData[product],
-        });
+        readProductImages(
+            path.join(__dirname, global.productsData[product].images_location)
+        )
+            .then((images) => {
+                console.log(images);
+                res.render("product", {
+                    title: `${global.productsData[product]["display_name"]} - SenaThenu Shop`,
+                    images: images,
+                    ...global.productsData[product],
+                });
+            })
+            .catch((err) => {
+                res.render(500);
+            });
     } else {
         res.render("errors/404");
     }
