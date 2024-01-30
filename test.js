db17fa9077a785300bbd9e47a01242ed8c8e8aea
Senash Thenuja <senathenu@gmail.com>
2024-01-30 23:25:39 +1300

    removed a test file

--- test.js
+++
@@ -1,7 +0,0 @@
-function isImageFile(filename) {
-    const imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"];
-    const ext = filename.slice(filename.indexOf("."));
-    return imageExtensions.includes(ext);
-}
-
-console.log(isImageFile("whatever.png"));
